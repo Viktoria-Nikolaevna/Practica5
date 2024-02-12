7edbc909ae44fe4bc4a1d38c2abbfdd9cc4a714d
StrelnikovaVN <v9299990393@gmail.com>
2024-02-12 15:29:55 +0300

    Выполнен ввод значения x.

--- ПР6/Program.cs
+++ ПР6/Program.cs
@@ -10,6 +10,9 @@ namespace ПР6
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите значения параметра x = ");
+            string s = Console.ReadLine();
+            double x = Convert.ToDouble(s);
         }
     }
 }
