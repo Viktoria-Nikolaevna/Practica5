2abc31ef7e313e4ac6b80e39314526d4c156ef4f
StrelnikovaVN <v9299990393@gmail.com>
2024-02-12 15:40:46 +0300

    Выполнен расчет значения F

--- ПР6/Program.cs
+++ ПР6/Program.cs
@@ -13,6 +13,8 @@ namespace ПР6
             Console.Write("Введите значения параметра x = ");
             string s = Console.ReadLine();
             double x = Convert.ToDouble(s);
+
+            double F = 2 * Math.Cos(x);
         }
     }
 }
